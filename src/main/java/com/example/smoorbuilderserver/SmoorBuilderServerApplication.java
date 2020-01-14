8bd7c948a62ac9844181deb5ca98231edf6f3ce7
Eric West <eawest2@gmail.com>
2020-01-14 16:41:46 -0500

    setup springboot app with spring intitializr

---
+++ src/main/java/com/example/smoorbuilderserver/SmoorBuilderServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.smoorbuilderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoorBuilderServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmoorBuilderServerApplication.class, args);
+	}
+
+}
